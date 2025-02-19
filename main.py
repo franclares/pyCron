968a9bdb05edbb514823d775e3a8c468a73eebb8
fclar <dam54@gmail.com>
2025-02-19 18:34:33 +0100

    añadir tarea con parametros

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ cron = CronTab(user=True)
 job = cron.new(command='mkdir -p $HOME/microntab')
 
 # Configurar para que se ejecute cada minuto
-job.minute.every(1)
+job.minute.on(35)
+job.hour.on(18)
+job.day.on(19)
+job.month.on(2)
 
 # Guardar cambios
 cron.write()
