4fa4105a70fb7f64a34bb7a6f91fa17ef10a0d33
fclar <dam54@gmail.com>
2025-02-19 18:43:13 +0100

    añadir tarea con parametroos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cron = CronTab(user=True)
 job = cron.new(command='export DISPLAY=:0 && export XDG_RUNTIME_DIR=/run/user/1000 && dunstify -u critical --icon="info" --appname="Nombre del proceso" "Título" "Mensaje"')
 
 # Configurar para que se ejecute cada minuto
-job.minute.on(35)
+job.minute.on(45)
 job.hour.on(18)
 job.day.on(19)
 job.month.on(2)
